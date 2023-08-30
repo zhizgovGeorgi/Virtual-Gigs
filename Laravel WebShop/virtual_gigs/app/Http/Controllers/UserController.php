e864eaf5e2b3c070d2fcaaefc734255e1dff314e
Georgi Zhizgov <i463263@fhict.nl>
2023-08-30 10:46:07 +0300

    Register functionality

---
+++ Laravel WebShop/virtual_gigs/app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+    //Show register form
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    //store new user
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'confirmed', 'min:6'],
+        ]);
+
+        //hash password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //create user
+        $user = User::create($formFields);
+
+        //Log in
+        auth()->login($user);
+
+
+        return redirect('/')->with('message', 'Successful registration!');
+    }
+}
--- Laravel WebShop/virtual_gigs/routes/web.php
+++ Laravel WebShop/virtual_gigs/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +37,13 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 //Show edit form 
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+//Show Register/Create form
+Route::get('/register', [UserController::class, 'create']);
+
+
+//Create new user
+Route::post('/users', [UserController::class, 'store']);
 
 
 
